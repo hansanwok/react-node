a85fff53b21f0e45dd0aba05a5ff011ab1d85fa9
Michael <dotrunghieu7696@gmail.com>
2018-02-11 16:58:41 +0700

    Init front end, setup route v4, absolute import path

---
+++ frontend/src/containers/A/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class A extends PureComponent { //esline-disabe-line
+  render() {
+    return (
+      <h1>Hello there I am Container A</h1>
+    );
+  }
+}
+
+export default A;
